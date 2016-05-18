65de8f2e6f60b0a28a5e6c04e1f0e6bd5d8581a9
AlexisNo <alexis@henaut.net>
2016-05-18 20:19:02 -0300

    Add command line to inspect API specification

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 'use strict';
 
 var lager = require('./lib/lager.js');
-var lambdaIntegration = require('./lib/plugins/lambda-integration');
 
 // Add the lambda integration plugin to Lager
+// @TODO plugins should be auto-registered
+var lambdaIntegration = require('./lib/plugins/lambda-integration');
 lager.registerPlugin(lambdaIntegration);
 
-// Retrieve the configuration from environment variables
-var region = process.env.AWS_DEFAULT_REGION || 'us-east-1';
-var stage = process.env.API_GATEWAY_STAGE || 'NO_STAGE';
-var environment = process.env.NODE_ENV || 'NO_ENV';
-
-module.exports = {
-  deploy: function() {
-    lager.deploy(region, environment, stage);
-  }
-};
+module.exports = lager;
