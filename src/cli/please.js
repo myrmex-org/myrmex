0c48ecefb3530096dfce1c3cf6a4f37d266e4487
Alexis N-o <alexis@henaut.net>
2016-06-24 01:55:46 -0300

    Improve "lager please"

--- src/cli/please.js
+++ src/cli/please.js
@@ -10,30 +10,97 @@ const Promise = lager.import.Promise;
 const figlet = Promise.promisify(require('figlet'));
 figlet.fonts = Promise.promisify(figlet.fonts);
 
+const cheers = {
+  'Afrikaans': 'Gesondheid',
+  'Albanian': 'Gëzuar',
+  'Arabic (Egypt)': 'Fe sahetek',
+  'Armenian (Western)': 'Genatzt',
+  'Azerbaijani': 'Nuş olsun',
+  'Bosnian': 'Živjeli',
+  'Bulgarian': 'Naz-dra-vey',
+  'Burmese': 'Aung myin par say',
+  'Catalan': 'Salut',
+  'Chamorro (Guam)': 'Biba',
+  'Chinese (Mandarin)': 'gān bēi',
+  'Croatian': 'Nazdravlje',
+  'Czech': 'Na zdravi',
+  'Danish': 'Skål',
+  'Dutch': 'Proost',
+  'English': 'Cheers',
+  'Estonian': 'Terviseks',
+  'Filipino/Tagalog': 'Mabuhay',
+  'Finnish': 'Kippis',
+  'French': 'Santé ',
+  'French2': 'A la votre',
+  'Galician': 'Salud',
+  'German': 'Prost',
+  'German2': 'Zum wohl',
+  'Greek': 'Yamas',
+  'Hawaiian': 'Okole maluna',
+  'Hebrew': 'L\'chaim',
+  'Hungarian': 'Egészségedre',
+  'Hungarian2': 'Fenékig',
+  'Icelandic': 'Skál',
+  'Irish Gaelic': 'Sláinte',
+  'Italian': 'Salute',
+  'Italian2': 'Cin cin',
+  'Japanese': 'Kanpai',
+  'Korean': 'Gun bae',
+  'Latvian': 'Priekā',
+  'Latvian2': 'Prosit',
+  'Lithuanian': 'į sveikatą',
+  'Norwegian': 'Skål',
+  'Polish': 'Na zdrowie',
+  'Portuguese': 'Saúde',
+  'Romanian': 'Noroc',
+  'Romanian2': 'Sanatate',
+  'Russian': 'Budem zdorovi',
+  'Russian2': 'Na zdorovie',
+  'Serbian': 'živeli',
+  'Slovak': 'Na zdravie',
+  'Slovenian': 'Na zdravje',
+  'Spanish': 'Salud',
+  'Swedish': 'Skål',
+  'Thai': 'Chok dee',
+  'Turkish': 'Serefe',
+  'Ukranian': 'Boodmo',
+  'Vietnamese': 'Môt hai ba, yo',
+  'Welsh': 'Iechyd da',
+  'Yiddish': 'Sei gesund',
+};
+
+const latinFonts = ['Big', 'Slant', 'Small', 'Small Slant', 'Standard', 'Block', 'Def Leppard', 'Efti Font', 'Efti Italic',
+               'Georgi16', 'Georgia11', 'Lean', 'Mini', 'Mirror', 'Mnemonic', 'Script', 'Shadow', 'Wavy'];
+
 /**
  * This module exports a function that enrich the interactive command line and return a promise
  * @returns {Promise} - a promise that resolve when the operation is done
  */
 module.exports = () => {
   // Build the lists of choices
-  const choicesLists = getChoices();
+  return getChoices()
+  .then(choicesLists => {
+    const config = {
+      section: 'Core plugin',
+      cmd: 'please',
+      description: 'create a new project',
+      options: { noHelp: true },
+      parameters: [{
+        cmdSpec: '--language <language>',
+        type: 'list',
+        choices: choicesLists.language
+      }, {
+        cmdSpec: '--font <font name>',
+        type: 'list',
+        choices: choicesLists.font
+      }]
+    };
 
-  const config = {
-    section: 'Core plugin',
-    cmd: 'please',
-    description: 'create a new project',
-    options: { noHelp: true },
-    parameters: [{
-      cmdSpec: '[--language]',
-      type: 'list',
-      choices: choicesLists.languages
-    }]
-  };
-
-  /**
-   * Create the command and the prompt
-   */
-  return icli.createSubCommand(config, executeCommand);
+    /**
+     * Create the command and the prompt
+     */
+    return icli.createSubCommand(config, executeCommand);
+  });
 };
 
 
@@ -43,15 +110,13 @@ module.exports = () => {
  * @returns {Object} - collection of lists of choices for "list" and "checkbox" parameters
  */
 function getChoices() {
-  return {
-    language: [
-      'french',
-      'english',
-      'spanish',
-      'german',
-      'portuguese'
-    ]
-  };
+  return figlet.fonts()
+  .then(font => {
+    return {
+      font,
+      language: _.keys(cheers)
+    };
+  });
 }
 
 /**
@@ -60,69 +125,12 @@ function getChoices() {
  * @returns {Promise<null>} - The execution stops here
  */
 function executeCommand(parameters) {
-  const allCheers = {
-    'Afrikaans': 'Gesondheid',
-    'Albanian': 'Gëzuar',
-    'Arabic (Egypt)': 'Fe sahetek',
-    'Armenian (Western)': 'Genatzt',
-    'Azerbaijani': 'Nuş olsun',
-    'Bosnian': 'Živjeli',
-    'Bulgarian': 'Naz-dra-vey',
-    'Burmese': 'Aung myin par say',
-    'Catalan': 'Salut',
-    'Chamorro (Guam)': 'Biba',
-    'Chinese (Mandarin)': 'gān bēi',
-    'Croatian': 'Nazdravlje',
-    'Czech': 'Na zdravi',
-    'Danish': 'Skål',
-    'Dutch': 'Proost',
-    'Estonian': 'Terviseks',
-    'Filipino/Tagalog': 'Mabuhay',
-    'Finnish': 'Kippis',
-    'French': 'Santé ',
-    'French2': 'A la votre',
-    'Galician': 'Salud',
-    'German': 'Prost',
-    'German2': 'Zum wohl',
-    'Greek': 'Yamas',
-    'Hawaiian': 'Okole maluna',
-    'Hebrew': 'L\'chaim',
-    'Hungarian': 'Egészségedre',
-    'Hungarian2': 'Fenékig',
-    'Icelandic': 'Skál',
-    'Irish Gaelic': 'Sláinte',
-    'Italian': 'Salute',
-    'Italian2': 'Cin cin',
-    'Japanese': 'Kanpai',
-    'Korean': 'Gun bae',
-    'Latvian': 'Priekā',
-    'Latvian2': 'Prosit',
-    'Lithuanian': 'į sveikatą',
-    'Norwegian': 'Skål',
-    'Polish': 'Na zdrowie',
-    'Portuguese': 'Saúde',
-    'Romanian': 'Noroc',
-    'Romanian2': 'Sanatate',
-    'Russian': 'Budem zdorovi',
-    'Russian2': 'Na zdorovie',
-    'Serbian': 'živeli',
-    'Slovak': 'Na zdravie',
-    'Slovenian': 'Na zdravje',
-    'Spanish': 'Salud',
-    'Swedish': 'Skål',
-    'Thai': 'Chok dee',
-    'Turkish': 'Şerefe',
-    'Ukranian': 'Boodmo',
-    'Vietnamese': 'Môt hai ba, yo',
-    'Welsh': 'Iechyd da',
-    'Yiddish': 'Sei gesund',
-  };
+  let word = parameters.language ? cheers[parameters.language] : _.sample(cheers);
   figlet.fonts()
   .then(fonts => {
-    const font = _.sample(fonts);
-    let cheers = _.sample(allCheers);
-    if (['default'].indexOf(font) === -1) { cheers = _.deburr(cheers); }
-    return figlet(cheers, { font });
+    const font = parameters.font ? parameters.font : _.sample(fonts);
+    if (latinFonts.indexOf(font) === -1) { word = _.deburr(word); }
+    return figlet(word, { font });
   })
   .then(cheers => {
     const b = txt => { return icli.format.custom(txt, '\x1b[93m'); };
