f3a1d54e33f518cf37c19284a4970015cdea2aa8
Alexis N-o <alexis@henaut.net>
2016-07-19 20:38:56 -0300

    Cleanup repo

--- dev-app/src/node-lambda/lambdas/sample-lambda-b/lambda.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var fn = require('./router.js');
-
-exports.handler = function(event, context) {
-  fn(event, context.done);
-};
--- dev-app/src/node-lambda/lambdas/sample-lambda-b/router.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = function(input, cb) {
-  var fn = require('./endpoints' + input.endpoint.path + '/' + input.endpoint.method);
-  fn(input, cb);
-};
