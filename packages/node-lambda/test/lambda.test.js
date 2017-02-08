d441b90e9f0305b4c784136b8c074a094bd1ecaa
AlexisNo <alexis@henaut.net>
2017-02-08 21:14:15 -0200

    Make the @lager/node-lambda test compatible with npm2

--- packages/node-lambda/test/lambda.test.js
+++ packages/node-lambda/test/lambda.test.js
@@ -131,7 +131,7 @@ describe('A Lambda', () => {
     assert.equal(example.key3, 'value3');
   });
 
-  it.skip('should be executed locally', () => {
+  it('should be executed locally', () => {
     return lambda.executeLocally({ a: 'b' })
     .then(res => {
       assert.equal(res.msg, 'This Lambda is not implemented!');
@@ -147,7 +147,7 @@ describe('A Lambda', () => {
     });
   });
 
-  describe.skip('deployment', () => {
+  describe('deployment', () => {
 
     it('should create the Lambda for the first deployment', function() {
       this.timeout(10000);
