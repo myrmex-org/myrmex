4bb5375ed0183b3ee86f12f40a91e7730c44b448
Alexis N-o <alexis@henaut.net>
2017-04-25 20:39:48 -0300

    Fix lint config (#62)

--- demo/express-app/app.js
+++ demo/express-app/app.js
@@ -1,6 +1,7 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
