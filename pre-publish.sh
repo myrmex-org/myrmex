1af35f5bef4cb236038926e9c205dbe89c34b3aa
Alexis N-o <alexis@henaut.net>
2017-05-04 01:56:37 -0300

    update dependencies and publish

--- pre-publish.sh
+++ pre-publish.sh
@@ -1,7 +1,7 @@
-lerna publish --skip-git --skip-npm
 lerna clean --yes
 lerna exec rm npm-shrinkwrap.json
 lerna exec ncu -- -u
 lerna exec npm install
+lerna publish --skip-git --skip-npm
 lerna exec npm shrinkwrap
 npm test
