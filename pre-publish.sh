2a998fd0c7d723ed3e187287b1912c9d88b025a6
AlexisNo <alexis@henaut.net>
2018-04-08 20:48:28 +0200

    chore(lerma): remove independent releases

--- pre-publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-lerna clean --yes
-lerna exec rm npm-shrinkwrap.json -- -f
-lerna exec ncu -- -u
-lerna exec npm install
-lerna publish --skip-git --skip-npm
-lerna exec npm shrinkwrap
-npm test
